b99d007c2b01ce2f7b1082a52ed37cb90577174f
Gabe Berke-Williams <gabebw@gabebw.com>
2015-06-28 23:33:18 -0400

    Beat level 3

--- player.rb
+++ player.rb
@@ -1,10 +1,50 @@
 class Player
+  MAX_HEALTH = 20
+
+  def initialize
+    @attacking = false
+  end
+
   def play_turn(warrior)
-    space = warrior.feel
-    if space.enemy?
-      warrior.attack!
+    @warrior = warrior
+
+    if unhealthy? && just_killed_something?
+      rest!
+    elsif space.enemy?
+      attack!
+    elsif done_resting?
+      @resting = false
+      play_turn(warrior)
     else
       warrior.walk!
     end
+  end
+
+  private
+
+  def unhealthy?
+    @warrior.health < MAX_HEALTH
+  end
+
+  def just_killed_something?
+    @attacking && ! space.enemy?
+  end
+
+  def done_resting?
+    @resting && ! unhealthy?
+  end
+
+  def rest!
+    @resting = true
+    @warrior.rest!
+  end
+
+  def attack!
+    @attacking = true
+    @warrior.attack!
+  end
+
+  def space
+    @warrior.feel
   end
 end
